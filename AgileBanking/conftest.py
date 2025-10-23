a187e019748a990179e2a2376f2a31a9eaf2d3aa
Sushma <sushmasdodamani725@gmail.com>
2025-10-23 14:36:53 +0530

    cicd

--- AgileBanking/conftest.py
+++ AgileBanking/conftest.py
@@ -1,17 +1,124 @@
+# # # import pytest
+# # # from selenium import webdriver
+# # # from selenium.webdriver.chrome.service import Service
+# # # from selenium.webdriver.chrome.options import Options
+# # # from webdriver_manager.chrome import ChromeDriverManager
+# # #
+# # # @pytest.fixture(scope="session")
+# # # def driver():
+# # #     options = Options()
+# # #     options.add_argument("--start-maximized")
+# # #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# # #     yield driver
+# # #     driver.quit()
+# # #
+# # # @pytest.fixture(scope="session")
+# # # def base_url():
+# # #     return "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+# # import pytest
+# # from selenium import webdriver
+# # from selenium.webdriver.chrome.service import Service as ChromeService
+# # from selenium.webdriver.edge.service import Service as EdgeService
+# # from selenium.webdriver.chrome.options import Options as ChromeOptions
+# # from selenium.webdriver.edge.options import Options as EdgeOptions
+# # from webdriver_manager.chrome import ChromeDriverManager
+# # from webdriver_manager.microsoft import EdgeChromiumDriverManager
+# #
+# #
+# # def pytest_addoption(parser):
+# #     parser.addoption("--browser", action="store", default="chrome", help="Browser to use: chrome or edge")
+# #
+# #
+# # @pytest.fixture(scope="session")
+# # def driver(request):
+# #     browser = request.config.getoption("--browser")
+# #
+# #     if browser.lower() == "chrome":
+# #         options = ChromeOptions()
+# #         options.add_argument("--start-maximized")
+# #         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+# #
+# #     elif browser.lower() == "edge":
+# #         options = EdgeOptions()
+# #         options.add_argument("--start-maximized")
+# #         driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+# #
+# #     else:
+# #         raise ValueError(f"Unsupported browser: {browser}")
+# #
+# #     yield driver
+# #     driver.quit()
+# #
+# #
+# # @pytest.fixture(scope="session")
+# # def base_url():
+# #     return "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+# import pytest
+# from utils.browser_setup import Browser
+#
+# BASE_URL = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+#
+#
+# # ✅ This will run tests for all 3 browsers automatically
+# def pytest_generate_tests(metafunc):
+#     if "browser_name" in metafunc.fixturenames:
+#         metafunc.parametrize("browser_name", ["chrome", "firefox", "edge"])
+#
+#
+# @pytest.fixture(scope="session")
+# def driver(browser_name):
+#     """Creates a WebDriver instance for each browser (Chrome, Firefox, Edge)."""
+#     browser = Browser(browser_name)
+#     driver = browser.get_driver()
+#     yield driver
+#     driver.quit()
+#
+#
+# @pytest.fixture(scope="session")
+# def base_url():
+#     return BASE_URL
+
+
+# conftest.py
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+#from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-@pytest.fixture(scope="session")
-def driver():
-    options = Options()
-    options.add_argument("--start-maximized")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# ✅ List of browsers to run sequentially
+@pytest.fixture(params=["chrome","firefox"], scope="function")
+def browser_name(request):
+    return request.param
+
+
+@pytest.fixture(scope="function")
+def driver(browser_name):
+    if browser_name == "chrome":
+        options = Options()
+        options.add_argument("--start-maximized")
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    elif browser_name == "firefox":
+         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
+    # elif browser_name == "edge":
+    #     driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+    else:
+        raise ValueError(f"Unsupported browser: {browser_name}")
+
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
+
 
 @pytest.fixture(scope="session")
 def base_url():
     return "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login"
+
--- AgileBanking/utils/browser_setup.py
+++ AgileBanking/utils/browser_setup.py
@@ -1,8 +1,38 @@
-# utils/browser_setup.py
+# # utils/browser_setup.py
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.options import Options
+# import pytest
+#
+# class Browser:
+#     def __init__(self, browser_name="chrome"):
+#         self.browser_name = browser_name.lower()
+#
+#     def get_driver(self):
+#         if self.browser_name == "chrome":
+#             chrome_options = Options()
+#             chrome_options.add_argument("--start-maximized")
+#             chrome_options.add_argument("--disable-notifications")
+#             chrome_options.add_argument("--disable-popup-blocking")
+#             service = Service()
+#             driver = webdriver.Chrome(service=service, options=chrome_options)
+#         elif self.browser_name == "firefox":
+#             driver = webdriver.Firefox()
+#         else:
+#             raise ValueError(f"Unsupported browser: {self.browser_name}")
+#         driver.implicitly_wait(10)
+#         return driver
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-import pytest
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 class Browser:
     def __init__(self, browser_name="chrome"):
@@ -10,15 +40,26 @@ class Browser:
 
     def get_driver(self):
         if self.browser_name == "chrome":
-            chrome_options = Options()
-            chrome_options.add_argument("--start-maximized")
-            chrome_options.add_argument("--disable-notifications")
-            chrome_options.add_argument("--disable-popup-blocking")
-            service = Service()
-            driver = webdriver.Chrome(service=service, options=chrome_options)
+            options = ChromeOptions()
+            options.add_argument("--start-maximized")
+            options.add_argument("--disable-notifications")
+            options.add_argument("--disable-popup-blocking")
+            service = ChromeService(ChromeDriverManager().install())
+            driver = webdriver.Chrome(service=service, options=options)
+
+        elif self.browser_name == "edge":
+            options = EdgeOptions()
+            options.add_argument("--start-maximized")
+            service = EdgeService(EdgeChromiumDriverManager().install())
+            driver = webdriver.Edge(service=service, options=options)
+
         elif self.browser_name == "firefox":
-            driver = webdriver.Firefox()
+            options = FirefoxOptions()
+            service = FirefoxService(GeckoDriverManager().install())
+            driver = webdriver.Firefox(service=service, options=options)
+
         else:
             raise ValueError(f"Unsupported browser: {self.browser_name}")
+
         driver.implicitly_wait(10)
         return driver
